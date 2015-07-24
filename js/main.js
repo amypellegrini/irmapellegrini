55b28706753f1cb2f7a18a509b2312ca66797f38
Amy Pellegrini <vicesaraco@gmail.com>
2015-07-24 19:35:00 -0300

    Contact module finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+(function() {
+  var contactBtn = $('#contact');
+      contactForm = $('#contact-form');
 
+  contactBtn.on('click', function(ev) {
+    ev.preventDefault();
+    $(contactForm).removeClass('hidden');
+    return false;
+  });
+
+  contactForm.find('form').parsley();
+}());
