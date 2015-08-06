c03c540a4d60813bf94338128c8d5001d5be0465
Amy Pellegrini <vicesaraco@gmail.com>
2015-08-06 17:13:47 -0300

    Fix contact event handlers.

--- js/main.js
+++ js/main.js
@@ -1,12 +1,25 @@
 (function() {
-  var contactBtn = $('#contact');
-      contactForm = $('#contact-form');
+  var
+      contactBtn = $('#contact'),
+      contactForm = $('#contact-form'),
 
-  contactBtn.on('click', function(ev) {
-    ev.preventDefault();
-    $(contactForm).removeClass('hidden');
-    return false;
-  });
+      hideContactForm = function(ev) {
+        if (ev.target === contactForm[0] ||
+            $(ev.target).closest('#contact-form')[0]) {
+          ev.stopPropagation();
+          return false;
+        }
+        $(contactForm).addClass('hidden');
+      },
 
+      showContactForm = function(ev) {
+        ev.preventDefault();
+        $(contactForm).removeClass('hidden');
+
+        return false;
+      };
+
+  contactBtn.on('click', showContactForm);
+  $(document).on('click', hideContactForm);
   contactForm.find('form').parsley();
 }());
